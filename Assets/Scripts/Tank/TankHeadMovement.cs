8873e06ada565d2cfc66f96ab015cb69bdcd8913
Edmundo Gonzalez Hernandez <mundo2599@outlook.com>
2019-11-18 18:50:51 -0600

    Se agrego TankVR y gira la cabeza del tanque

---
+++ Assets/Scripts/Tank/TankHeadMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TankHeadMovement : MonoBehaviour
+{
+    public GameObject m_TankHead;
+    public float m_Sensitivity = 50f;
+    public float m_MaxXRotate = 45f;
+    public float m_MaxYRotate = 45f;
+
+    private float m_Xvalue;
+    private float m_Yvalue;
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+
+    private void Update()
+    {
+        m_Xvalue = Input.GetAxis("Mouse X");
+        m_Yvalue = Input.GetAxis("Mouse Y");
+    }
+
+    private void FixedUpdate()
+    {
+        yRotation += m_Xvalue * m_Sensitivity * Time.deltaTime;
+        xRotation -= m_Yvalue * m_Sensitivity * Time.deltaTime;
+
+        // xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -m_MaxXRotate, m_MaxXRotate);
+        yRotation = Mathf.Clamp(yRotation, -m_MaxYRotate, m_MaxYRotate);
+
+        m_TankHead.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+
+}
