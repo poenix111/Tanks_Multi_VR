a04f0b7a109954f8f834edc4c455f7459552f2a2
Edmundo Gonzalez <mundo2599@outlook.com>
2019-11-28 20:09:40 -0600

    Se agrego escena de objetivos con 3 objetivos

---
+++ Assets/Scripts/Managers/ObjetivosManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetivosManager : MonoBehaviour
+{
+
+    public GameObject m_ObjetivoPrefab;
+    public Transform[] m_SpawnPoints;
+
+    private List<GameObject> m_Objetivos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Objetivos = new List<GameObject>();
+        foreach (Transform trans in m_SpawnPoints)
+        {
+            Instantiate(m_ObjetivoPrefab, trans.position, trans.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Objetivo/ManageHit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageHit : MonoBehaviour
+{
+    // Metodo que se llama cuando es golpeado el cubo
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
